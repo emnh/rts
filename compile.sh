3fd28ffcdc38d7d24a3d9402db640686c0069a4a
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2015-07-16 15:58:04 +0200

    add step to generate firefox-compatible es6 to bundle2.js

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 node node_modules/jade/bin/jade.js -P index.jade
 node ./node_modules/browserify/bin/cmd.js index.js -t babelify --outfile bundle.js
+# for debugging es6 for..of in firefox, gives better errors than babel translation
+# node ./node_modules/browserify/bin/cmd.js index.js -t [ babelify --whitelist es6.modules,es6.constants ] --outfile bundle2.js
