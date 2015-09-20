52d34abe76519b0190e4b1b2a99ca26f43cd7236
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2015-09-20 13:52:33 +0200

    instanced rendering for explosions.

--- shaders/explosion-fragment.glsl
+++ shaders/explosion-fragment.glsl
@@ -1,8 +1,8 @@
-varying vec2 vUv;
+precision highp float;
+
 varying float noise;
 uniform sampler2D tExplosion;
-uniform float time;
-uniform float opacity;
+varying float v_opacity;
 
 float random( vec3 scale, float seed ){
   return fract( sin( dot( gl_FragCoord.xyz + seed, scale ) ) * 43758.5453 + seed ) ;
@@ -12,5 +12,5 @@ void main() {
   float r = 0.01 * random( vec3( 12.9898, 78.233, 151.7182 ), 0.0);
   vec2 tPos = vec2(0.0, 2.0 * noise + r);
   vec4 color = texture2D(tExplosion, tPos);
-  gl_FragColor = vec4(color.rgb, opacity);
+  gl_FragColor = vec4(color.rgb, v_opacity);
 }
