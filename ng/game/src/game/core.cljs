c159f24352cd695e0d41306cc2992b475db30788
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2015-10-02 07:35:42 +0200

    add template for next generation of the game

---
+++ ng/game/src/game/core.cljs
@@ -0,0 +1,27 @@
+(ns ^:figwheel-always game.core
+    (:require[om.core :as om :include-macros true]
+              [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console: test.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello pretty world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text data)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  (swap! app-state update-in [:__figwheel_counter] inc)
+)
+
