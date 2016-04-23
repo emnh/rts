43522f33c6a67fbe878f3d94c209ad9ddf3c0156
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-04-23 19:35:47 +0200

    change paths

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ./scripts/getModelSizes.py > json/modelSizes.json
 # node node_modules/jade/bin/jade.js -P index.jade
-if [ deps.js -nt bundle-deps.js ]; then
-  node ./node_modules/browserify/bin/cmd.js deps.js --ig --fast --outfile bundle-deps.js
+if [ js/deps.js -nt resources/public/bundle-deps.js ]; then
+  node \
+    ./node_modules/browserify/bin/cmd.js \
+    js/deps.js --ig --fast --outfile \
+    resources/public/bundle-deps.js
 fi
