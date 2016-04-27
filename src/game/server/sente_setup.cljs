b2c023ce47dc39d64e9846657123581350f0cb52
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-04-27 17:40:39 +0200

    fix sente server reload among other

--- src/game/server/sente_setup.cljs
+++ src/game/server/sente_setup.cljs
@@ -32,6 +32,12 @@
   (-event-msg-handler component ev-msg))
 
 (defmethod -event-msg-handler
+  :chsk/uidport-open
+  [component {:as ev-msg :keys [event id ?data uid ring-req ?reply-fn send-fn]}]
+  (println "uidport-open" uid)
+  (send-fn uid [:sente-test/uidport [:hello uid]]))
+
+(defmethod -event-msg-handler
   :sente-test/ping
   [component {:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
   (let [session (:session ring-req)
@@ -39,6 +45,12 @@
     ;(println "ping")
     (when ?reply-fn
       (?reply-fn [ :sente-test/pong (:test component) ?data ]))))
+
+; catch it so it's not logged (too many of them)
+(defmethod -event-msg-handler
+  :chsk/ws-ping
+  [component {:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
+  nil)
 
 (defmethod -event-msg-handler
   :default ; Default/fallback case (no other matching handler)
@@ -49,62 +61,60 @@
     (when ?reply-fn
       (?reply-fn {:umatched-event-as-echoed-from-from-server event}))))
 
+(defn user-id-fn
+  [req]
+  (let
+    [uid (db/get-id (-> req :body .-session .-user .-_id))]
+    uid))
+
 (defcom
   new-sente-setup
   [app server]
   [router ajax-post-fn ajax-get-or-ws-handshake-fn ch-recv send-fn connected-uids]
   (fn [component]
-		(let [;; Serialization format, must use same val for client + server:
-					packer :edn ; Default packer, a good choice in most cases
-					;; (sente-transit/get-flexi-packer :edn) ; Experimental, needs Transit dep
-          ws-server (express-ws (:app app) (:server server))
-					{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn
-									connected-uids]}
-          (if
-            (= start-count 0)
-            (sente-express/make-express-channel-socket-server! 
-              {
-               :packer packer
-               :user-id-fn (fn [req] 
-                             (let
-                               [uid (db/get-id (-> req :body .-session .-user .-_id))]
-                               ;(println "uid" uid)
-                               uid))
-               })
-            component)
-					router (if (= router nil) (atom nil) router)
-          component
-          (->
-            component
-            (assoc :test { :component-start-time (-> (new js/Date) .toString) })
-            (assoc :router router)
-            (assoc :ajax-post-fn                ajax-post-fn)
-            (assoc :ajax-get-or-ws-handshake-fn ajax-get-or-ws-handshake-fn)
-            (assoc :ch-recv                     ch-recv) ; ChannelSocket's receive channel
-            (assoc :send-fn                     send-fn) ; ChannelSocket's send API fn
-            (assoc :connected-uids              connected-uids) ; Watchable, read-only atom
-            )
-					stop-router! #(when-let [stop-f @router] (stop-f))
-					start-router!
-						#(do
-							(stop-router!)
-							(reset! router
-											(sente/start-server-chsk-router!
-											 ch-recv (partial event-msg-handler component))))
-          ]
-         (doto
-           (:app app)
-           (.ws "/chsk"
-                (fn [ws req next]
-                  ;(println "ws chsk")
-                  (ajax-get-or-ws-handshake-fn req nil nil
-                                            {:websocket? true
-                                             :websocket  ws})))
-           (.get "/chsk" ajax-get-or-ws-handshake-fn)
-           (.post "/chsk" ajax-post-fn))
-         ;(if (= @router nil) (start-router!))
-         (start-router!)
-         component))
-  (fn [component]
-    component)
-  )
+		(let 
+      [packer :edn ; Default packer, a good choice in most cases
+       _ 
+       (if
+         (= start-count 0)
+         (express-ws (:app app) (:server server)))
+       {:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn
+               connected-uids]}
+       (if
+         (= start-count 0)
+         (sente-express/make-express-channel-socket-server! 
+           { :packer packer :user-id-fn user-id-fn })
+         component)
+       router (if (= router nil) (atom nil) router)
+       component
+       (->
+         component
+         (assoc :test { :component-start-time (-> (new js/Date) .toString) })
+         (assoc :router router)
+         (assoc :ajax-post-fn                ajax-post-fn)
+         (assoc :ajax-get-or-ws-handshake-fn ajax-get-or-ws-handshake-fn)
+         (assoc :ch-recv                     ch-recv) ; ChannelSocket's receive channel
+         (assoc :send-fn                     send-fn) ; ChannelSocket's send API fn
+         (assoc :connected-uids              connected-uids) ; Watchable, read-only atom
+         )
+       stop-router! #(when-let [stop-f @router] (stop-f))
+       start-router!
+         #(do
+           (stop-router!)
+           (reset! router
+                   (sente/start-server-chsk-router!
+                    ch-recv (partial event-msg-handler component))))
+       ]
+       (doto
+         (:app app)
+         (.ws "/chsk"
+              (fn [ws req next]
+                ;(println "ws chsk")
+                (ajax-get-or-ws-handshake-fn req nil nil
+                                          {:websocket? true
+                                           :websocket  ws})))
+         (.get "/chsk" ajax-get-or-ws-handshake-fn)
+         (.post "/chsk" ajax-post-fn))
+       (start-router!)
+       component))
+  (fn [component] component))
