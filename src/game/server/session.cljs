2910ac6a91197aa05d27f73c47a553b800b00a97
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-04-28 12:07:33 +0200

    reuse session

--- src/game/server/session.cljs
+++ src/game/server/session.cljs
@@ -13,29 +13,32 @@
 
 (defn start
   [component]
-  (let
-    [config (:config component)
-     session-secret (get-in config [:session :secret])
-     url (get-in config [:db :url])
-     store
-      (new MongoDBStore #js { 
-         ; TODO: reuse db connection?
-         :uri url 
-         :collection "mySessions"
-         })
-     _ (-> store (.on "error" 
-                      (fn [error]
-                        (-> assert (.ifError error)))))
-     session
-       (session
-          #js { 
-              :secret session-secret 
-              :key "express.sid"
-              :resave false
-              :saveUninitialized false
-              :store store
-              })]
-    (assoc component :session session)))
+  (if 
+    (= (:start-count component) 0)
+    (let
+      [config (:config component)
+       session-secret (get-in config [:session :secret])
+       url (get-in config [:db :url])
+       store
+        (new MongoDBStore #js { 
+           ; TODO: reuse db connection?
+           :uri url 
+           :collection "mySessions"
+           })
+       _ (-> store (.on "error" 
+                        (fn [error]
+                          (-> assert (.ifError error)))))
+       session
+         (session
+            #js { 
+                :secret session-secret 
+                :key "express.sid"
+                :resave false
+                :saveUninitialized false
+                :store store
+                })]
+      (assoc component :session session))
+    component))
 
 (defn stop
   [component]
