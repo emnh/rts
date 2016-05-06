4fde48a962a0d92b2c5025135af5b4326d90bb16
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-05-06 22:15:10 +0200

    work on health bars

--- src.client/game/client/overlay.cljs
+++ src.client/game/client/overlay.cljs
@@ -22,20 +22,23 @@
   (let
     [health-bars (:health-bars component)
      t (common/game-time)
-     screen-boxes (selection/get-screen-boxes component)]
+     screen-boxes (selection/get-screen-boxes component)
+     line-width 2]
     (-> health-bars .clear)
-    (-> health-bars (.lineStyle 1 0x000000 1))
+    (-> health-bars (.lineStyle line-width 0x000000 1))
     (doseq
       [[i box] (map-indexed vector screen-boxes)]
       (let
         [mesh (aget box "mesh")
          unit (engine/get-unit-for-mesh (:units component) mesh)
+         bar-height 8
+         bar-block-width 12
          [x1 y1 x2 y2] box
          width (- x2 x1)
+         width (max width (* 5 bar-block-width))
          height (- y2 y1)
 ;         health (/ (inc i) (count screen-boxes))
          health (/ (:health unit) (:max-health (:model unit)))
-         bar-height 8
          health-width (* health width)
          bar-width width
          y1 (- y1 bar-height)
@@ -54,8 +57,10 @@
         (-> health-bars
           (.drawRect x1 y1 health-width bar-height))
         (-> health-bars .endFill)
-        (-> health-bars
-          (.drawRect x1 y1 width bar-height))))))
+        (doseq
+          [i (range bar-block-width width bar-block-width)]
+          (-> health-bars
+            (.drawRect x1 y1 i bar-height)))))))
 
 (defcom
   new-overlay
