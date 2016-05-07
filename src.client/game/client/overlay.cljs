f65199b1bc28b11d0ca50d779a8f46a0129f47da
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2016-05-07 18:52:31 +0200

    add frame statistics. add fast but ugly version of health bars. a better fast version is possible.

--- src.client/game/client/overlay.cljs
+++ src.client/game/client/overlay.cljs
@@ -19,13 +19,171 @@
     [game.shared.macros :as macros :refer [defcom]])
   )
 
+(def shader
+"
+precision mediump float;
+
+varying vec2 vTextureCoord;
+varying vec4 vColor;
+
+uniform sampler2D uSampler;
+uniform float gray;
+
+void main(void)
+{
+  vec4 rgba = texture2D(uSampler, vTextureCoord);
+  vec4 rgba_orig = rgba;
+  const float block_width = 12.0;
+  float width = gl_FragCoord.x / vTextureCoord.x;
+  float height = gl_FragCoord.y / vTextureCoord.y;
+  vec4 neighbourTop = texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y - 1.0 / height));
+  vec4 neighbourTop2 = texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y - 2.0 / height));
+  vec4 neighbourBottom = texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y + 1.0 / height));
+  vec4 neighbourBottom2 = texture2D(uSampler, vec2(vTextureCoord.x, vTextureCoord.y + 2.0 / height));
+  float x = vTextureCoord.x;
+  float y = vTextureCoord.y;
+  //float localX = gl_FragCoord.x - rgba.b * width;
+  float localX = 0.0; // gl_FragCoord.x; // - rgba.b * 12.0;
+
+  for (float i = 0.0; i < 20.0; i += 1.0) {
+    float offset = i * block_width;
+    vec4 left = texture2D(uSampler, vec2(vTextureCoord.x - offset / width, vTextureCoord.y));
+    if (left.a == 0.0) {
+      localX = offset;
+      break;
+    }
+  }
+  float maxX = 0.0;
+  for (float i = 0.0; i < 20.0; i += 1.0) {
+    float offset = i * block_width;
+    vec4 left = texture2D(uSampler, vec2(vTextureCoord.x + offset / width, vTextureCoord.y));
+    if (left != rgba_orig) {
+      maxX = offset;
+      break;
+    }
+  }
+  float bar_width = maxX + localX;
+
+  const float eps = 1.0e-7;
+  float offsetX = localX / bar_width;
+  float health = rgba_orig.b;
+  float health_width = health * bar_width;
+  float remainder = mod(health * bar_width, block_width);
+  health_width -= remainder;
+  float last_block_opacity = remainder / block_width;
+  rgba.b = 0.0;
+  vec3 color = rgba_orig.rgb;
+  if (health < 0.25) {
+    color = vec3(1.0, 0.0, 0.0);
+  } else if (health < 0.5) {
+    color = vec3(1.0, 165.0 / 255.0, 0.0);
+  } else if (health < 0.75) {
+    color = vec3(1.0, 1.0, 0.0);
+  } else {
+    color = vec3(0.0, 1.0, 0.0);
+  }
+  float blockX = gl_FragCoord.x;
+  bool transparent = false;
+  if (rgba.a > 0.0) {
+    if (localX <= health_width) {
+      rgba.rgb = color;
+    } else if (localX - 12.0 <= health_width) {
+      rgba.rgb = color;
+      rgba.a = last_block_opacity;
+    } else {
+      rgba = vec4(0.0);
+      transparent = true;
+    }
+    // block separator
+    if (mod(blockX, 12.0) < 1.0) {
+      rgba.rgba = vec4(vec3(0.0), 1.0);
+    }
+    // shadow left
+    if (mod(blockX - 1.0, 12.0) < 1.0) {
+      if (transparent) {
+        rgba = vec4(vec3(0.0), 0.1);
+      } else {
+        rgba.rgb = mix(color, vec3(0.0), 0.1);
+      }
+    }
+    //shadow right
+    if (mod(blockX + 1.0, 12.0) < 1.0) {
+      if (transparent) {
+        rgba = vec4(vec3(0.0), 0.1);
+      } else {
+        rgba.rgb = mix(color, vec3(0.0), 0.1);
+      }
+    }
+    // border top
+    if (rgba_orig.a != neighbourTop.a) {
+      rgba = vec4(vec3(0.0), 1.0);
+    }
+    // border bottom
+    if (rgba_orig.a != neighbourBottom.a) {
+      rgba = vec4(vec3(0.0), 1.0);
+    }
+    // light top
+    if (neighbourTop.a != neighbourTop2.a) {
+      if (transparent) {
+        //rgba = vec4(vec3(1.0), 0.0);
+      } else {
+        rgba.rgb = mix(rgba.rgb, vec3(1.0), 0.2);
+      }
+    }
+    // shadow bottom
+    if (neighbourBottom.a != neighbourBottom2.a) {
+      if (transparent) {
+        rgba = vec4(vec3(0.0), 0.4);
+      } else {
+        rgba.rgb = mix(rgba.rgb, vec3(0.0), 0.4);
+      }
+    }
+  }
+  gl_FragColor = rgba;
+}
+")
+
+(defn get-pixi-filter
+  []
+  (let
+    [TestFilter
+     (fn []
+       (this-as
+         this
+         (-> js/PIXI.Filter
+           (.call this nil shader #js { :gray #js { :type "1f" :value 1 } }))))
+     ]
+    (set!
+      (-> TestFilter .-prototype)
+      (-> js/Object (.create (-> js/PIXI.Filter .-prototype))))
+    (set!
+      (-> TestFilter .-prototype .-constructor)
+      TestFilter)
+    (new js/TestFilter)))
+
 (defn on-render
-  [component]
+  [init-renderer component]
   (let
     [health-bars (:health-bars component)
      t (common/game-time)
-     screen-boxes (selection/get-screen-boxes component)]
-    (-> health-bars .clear)
+     screen-boxes (selection/get-screen-boxes component)
+     pixi-filter (get-pixi-filter)
+     stage (:stage component)
+     health-bars (new js/PIXI.Graphics)
+     scene-width @(get-in component [:scene-properties :width])
+     scene-height @(get-in component [:scene-properties :height])
+     last-frame-time @(:last-60-average init-renderer)
+     fast-version false ;(> last-frame-time (* 16.67 2))
+     ]
+;    (-> health-bars .clear)
+    (if fast-version
+      (-> stage .-filters (set! #js [pixi-filter])))
+    (-> stage .removeChildren)
+    (-> stage (.addChild health-bars))
+    ; draw invisible rectangle so filter works on whole screen
+    (-> health-bars (.lineStyle 0))
+    (-> health-bars
+      (.drawRect 0 0 scene-width scene-height))
     (doseq
       [[i box] (map-indexed vector screen-boxes)]
       (let
@@ -46,6 +204,7 @@
          bar-width (* bar-block-width (min (max (math/round (/ box-width bar-block-width)) min-blocks) max-blocks))
          ; center bar on box horizontally
          x1 (infix (box-width - bar-width) / 2 + x1)
+         x1 (- x1 (rem x1 bar-block-width))
          height (- y2 y1)
          health (/ (:health unit) (:max-health (:model unit)))
          health-width (* health bar-width)
@@ -63,49 +222,65 @@
            0xFFFF00
            :else
            0x00FF00)
+         ;color (+ color (* (rem x1 bar-block-width) 255))
          ]
-        ; full blocks
-        (-> health-bars (.lineStyle line-width 0x000000 1))
-        (-> health-bars (.beginFill color 1))
-        (-> health-bars
-          (.drawRect x1 y1 health-width bar-height))
-        (-> health-bars .endFill)
-        ; last transparent block
-        (-> health-bars (.beginFill color last-block-opacity))
-        (-> health-bars
-          (.drawRect (+ x1 health-width) y1 bar-block-width bar-height))
-        (-> health-bars .endFill)
-        ; top lighter
-        (-> health-bars (.lineStyle 0))
-        (-> health-bars (.beginFill 0xFFFFFF light-opacity))
-        (-> health-bars
-          (.drawRect x1 y1 bar-width shadow-height))
-        (-> health-bars .endFill)
-        ; bottom darker
-        (-> health-bars (.lineStyle 0))
-        (-> health-bars (.beginFill 0x000000 shadow-opacity))
-        (-> health-bars
-          (.drawRect x1 (+ y1 bar-height (- shadow-height)) bar-width shadow-height))
-        (-> health-bars .endFill)
-        (doseq
-          [i (range bar-block-width (inc bar-width) bar-block-width)]
-          ; left/right shadow
-          (-> health-bars (.lineStyle 0))
-          (-> health-bars (.beginFill 0x000000 shadow-opacity))
-          (-> health-bars
-            (.drawRect (+ x1 i (- bar-block-width)) y1 shadow-width bar-height))
-          (-> health-bars
-            (.drawRect (+ x1 i (- shadow-width)) y1 shadow-width bar-height))
-          (-> health-bars .endFill)
-          ; block borders
-          (-> health-bars (.lineStyle line-width 0x000000 1))
-          (-> health-bars
-            (.drawRect x1 y1 i bar-height)))))))
+        (if fast-version
+          (do
+            ; full blocks
+            (-> health-bars (.lineStyle 0))
+            (-> health-bars (.beginFill (* health 255) 1))
+            (-> health-bars
+              (.drawRect x1 y1 bar-width bar-height))
+            (-> health-bars .endFill)
+            )
+          (do
+            ; full blocks
+            (-> health-bars (.lineStyle 0))
+            (-> health-bars (.beginFill color 1))
+            (-> health-bars
+              (.drawRect x1 y1 health-width bar-height))
+            (-> health-bars .endFill)
+            ; last transparent block
+            (-> health-bars (.lineStyle 0))
+            (-> health-bars (.beginFill color last-block-opacity))
+            (-> health-bars
+              (.drawRect (+ x1 health-width) y1 bar-block-width bar-height))
+            (-> health-bars .endFill)
+            ; borders
+            (-> health-bars (.lineStyle line-width 0x000000 1))
+            (-> health-bars
+              (.drawRect x1 y1 bar-width bar-height))
+            ; top lighter
+            (-> health-bars (.lineStyle 0))
+            (-> health-bars (.beginFill 0xFFFFFF light-opacity))
+            (-> health-bars
+              (.drawRect x1 y1 bar-width shadow-height))
+            (-> health-bars .endFill)
+            ; bottom darker
+            (-> health-bars (.lineStyle 0))
+            (-> health-bars (.beginFill 0x000000 shadow-opacity))
+            (-> health-bars
+              (.drawRect x1 (+ y1 bar-height (- shadow-height)) bar-width shadow-height))
+            (-> health-bars .endFill)
+            (doseq
+              [i (range bar-block-width (inc bar-width) bar-block-width)]
+              ; left/right shadow
+              (-> health-bars (.lineStyle 0))
+              (-> health-bars (.beginFill 0x000000 shadow-opacity))
+              (-> health-bars
+                (.drawRect (+ x1 i (- bar-block-width)) y1 shadow-width bar-height))
+              (-> health-bars
+                (.drawRect (+ x1 i (- shadow-width)) y1 shadow-width bar-height))
+              (-> health-bars .endFill)
+              ; block borders
+              (-> health-bars (.lineStyle line-width 0x000000 1))
+              (-> health-bars
+                (.drawRect x1 y1 i bar-height)))))))))
 
 (defcom
   new-overlay
   [$overlay params units camera renderer scene-properties]
-  [pixi-renderer stage health-bars]
+  [pixi-renderer stage]
   (fn [component]
     (let
       [$container (:$page params)
@@ -114,7 +289,7 @@
        view (aget $overlay 0)
        width (-> $overlay .width)
        height (-> $overlay .height)
-       pixi-renderer 
+       pixi-renderer
        (or pixi-renderer
         (new js/PIXI.WebGLRenderer
              width
@@ -127,12 +302,9 @@
               :autoResize true
               }))
        stage (new js/PIXI.Container)
-       health-bars (new js/PIXI.Graphics)
        component
        (-> component
-         (assoc :health-bars health-bars)
          (assoc :stage stage)
          (assoc :pixi-renderer pixi-renderer))]
-      (-> stage (.addChild health-bars))
       component))
   (fn [component] component))
--- src.client/game/client/renderer.cljs
+++ src.client/game/client/renderer.cljs
@@ -13,7 +13,8 @@
 (defn render-loop
   [component]
   (let
-   [camera (data (:camera component))
+   [start-time (common/game-time)
+    camera (data (:camera component))
     scene (data (:scene component))
     renderer (data (:renderer component))
     render-stats (data (:render-stats component))
@@ -23,18 +24,32 @@
     ; TODO: generic component render
     (-> render-stats .update)
     (-> renderer (.render scene camera))
-    (overlay/on-render (:pixi-overlay component))
+    (overlay/on-render component (:pixi-overlay component))
     (-> pixi-renderer (.render pixi-stage))
+    (let
+      [end-time (common/game-time)
+       elapsed (- end-time start-time)]
+      (reset! (:last-frame-time component) elapsed)
+      (swap! (:last-60-frame-times component) #(conj (take 59 %) elapsed))
+      (reset! (:last-60-average component)
+             (/
+               (reduce + @(:last-60-frame-times component))
+               (count @(:last-60-frame-times component)))))
     (if @(:running component)
       (js/requestAnimationFrame (partial render-loop component)))))
 
 (defcom
   new-init-renderer
   [renderer camera scene render-stats pixi-overlay]
-  [running]
+  [running last-frame-time last-60-frame-times last-60-average]
   (fn [component]
     (let
-      [component (assoc component :running (atom true))]
+      [component
+       (-> component
+         (assoc :last-frame-time (atom 0))
+         (assoc :last-60-frame-times (atom []))
+         (assoc :last-60-average (atom 0))
+         (assoc :running (atom true)))]
       (render-loop component)
       component))
   (fn [component]
