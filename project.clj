7cb911ae8fe2b6f645a9c17144728eeaf8cb46e2
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2017-05-03 13:02:29 +0200

    update to my own gamma release with nested assignments fix

--- project.clj
+++ project.clj
@@ -17,7 +17,8 @@
                  [rm-hull/infix "0.2.11"]
                  [rum "0.8.2"]
                  [sablono "0.7.0"]
-                 [hendekagon/gamma "0.1.3"]
+                 ;[hendekagon/gamma "0.1.3"]
+                 [org.clojars.emh/gamma "0.0-135-24-ga976"]
                  [thheller/shadow-devtools "1.0.20170429-17"]]
 
   :plugins
